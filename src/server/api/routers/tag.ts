98dd62458f0d714a9f6da768de213d70296cde10
Richard Schwarcz <riso.schwarcz@gmail.com>
2024-07-19 20:43:04 +0200

    create tag router

---
+++ src/server/api/routers/tag.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
+
+export const tagRouter = createTRPCRouter({
+  createTag: publicProcedure
+    .input(
+      z.object({
+        tag: z.string(),
+        color: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.tag.create({
+        data: input,
+      })
+    }),
+  getTags: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.tag.findMany()
+  }),
+})
