75d783ab3f997b5b735b6e0dc3d863bbca694c4e
Richard Schwarcz <riso.schwarcz@gmail.com>
2024-07-20 11:06:15 +0200

    calculate height of the container, set width and flex none

--- src/components/new-task-sidebar.tsx
+++ src/components/new-task-sidebar.tsx
@@ -66,151 +66,156 @@ export default function NewTaskSidebar({
   })
 
   return (
-    <div className="flex min-h-screen w-1/3 flex-col justify-between rounded-3xl bg-stone-50 p-4">
-      <div>
-        <div className="mb-4 flex items-center justify-between">
-          <p className="text-xl font-bold">New Task</p>
-          <Button
-            size="icon"
-            variant="ghost"
-            onClick={() => setTaskbarVisibility(!isTaskbarVisible)}
-          >
-            <X />
-          </Button>
+    <div className="w-72 flex-none">
+      <div className="flex min-h-[calc(100vh-2rem)] flex-col justify-between rounded-3xl bg-stone-50 p-4">
+        <div>
+          <div className="mb-4 flex items-center justify-between">
+            <p className="text-xl font-bold">New Task</p>
+            <Button
+              size="icon"
+              variant="ghost"
+              onClick={() => setTaskbarVisibility(!isTaskbarVisible)}
+            >
+              <X />
+            </Button>
+          </div>
+
+          <MenuSeparator />
+
+          <form>
+            <div className="mb-4 flex flex-col gap-4">
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-slate-500">Task</p>
+                <Controller
+                  name="task"
+                  control={form.control}
+                  render={({ field }) => (
+                    <Input {...field} placeholder="New Task" />
+                  )}
+                />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-slate-500">
+                  Description
+                </p>
+                <Controller
+                  name="description"
+                  control={form.control}
+                  render={({ field }) => (
+                    <Textarea {...field} placeholder="Task Description" />
+                  )}
+                />
+              </div>
+            </div>
+
+            <MenuSeparator />
+
+            <div className="mb-4 flex flex-col">
+              <p className="text-sm font-semibold text-slate-500">List</p>
+              <Controller
+                control={form.control}
+                name="list"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pick A List" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {lists?.map((list) => {
+                        return (
+                          <SelectItem value={list.id} key={list.id}>
+                            {list.list}
+                          </SelectItem>
+                        )
+                      })}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
+
+            <div className="mb-4 flex flex-col">
+              <p className="text-sm font-semibold text-slate-500">Priority</p>
+              <Controller
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Choose priority" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {formSchema.shape.priority.options.map((priority) => {
+                        return (
+                          <SelectItem
+                            value={priority.value}
+                            key={priority.value}
+                          >
+                            {priority.value}
+                          </SelectItem>
+                        )
+                      })}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
+
+            <div className="mb-4 flex flex-col">
+              <p className="text-sm font-semibold text-slate-500">Due Date</p>
+              <DatePicker<z.infer<typeof formSchema>>
+                name="dueDate"
+                control={form.control}
+              />
+            </div>
+
+            <MenuSeparator />
+
+            <div className="flex flex-col">
+              <p className="text-sm font-semibold text-slate-500">Tags</p>
+              <Controller
+                control={form.control}
+                name="tags"
+                render={() => (
+                  <MultiSelect
+                    placeholder="Add Tags"
+                    defaultValue={[]}
+                    options={
+                      tags?.map((tag) => ({
+                        label: tag.tag,
+                        value: tag.id,
+                      })) ?? []
+                    }
+                    onValueChange={(values) => {
+                      const selectedOptions = values.map((value) => ({
+                        id: value,
+                        tag: tags?.find((tag) => tag.id === value)?.tag ?? '',
+                      }))
+                      form.setValue('tags', selectedOptions)
+                    }}
+                  />
+                )}
+              />
+            </div>
+          </form>
         </div>
 
-        <MenuSeparator />
-
-        <form>
-          <div className="mb-4 flex flex-col gap-4">
-            <div className="flex flex-col">
-              <p className="text-sm font-semibold text-slate-500">Task</p>
-              <Controller
-                name="task"
-                control={form.control}
-                render={({ field }) => (
-                  <Input {...field} placeholder="New Task" />
-                )}
-              />
-            </div>
-            <div className="flex flex-col">
-              <p className="text-sm font-semibold text-slate-500">
-                Description
-              </p>
-              <Controller
-                name="description"
-                control={form.control}
-                render={({ field }) => (
-                  <Textarea {...field} placeholder="Task Description" />
-                )}
-              />
-            </div>
-          </div>
-
-          <MenuSeparator />
-
-          <div className="mb-4 flex flex-col">
-            <p className="text-sm font-semibold text-slate-500">List</p>
-            <Controller
-              control={form.control}
-              name="list"
-              render={({ field }) => (
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Pick A List" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {lists?.map((list) => {
-                      return (
-                        <SelectItem value={list.id} key={list.id}>
-                          {list.list}
-                        </SelectItem>
-                      )
-                    })}
-                  </SelectContent>
-                </Select>
-              )}
-            />
-          </div>
-
-          <div className="mb-4 flex flex-col">
-            <p className="text-sm font-semibold text-slate-500">Priority</p>
-            <Controller
-              control={form.control}
-              name="priority"
-              render={({ field }) => (
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Choose priority" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {formSchema.shape.priority.options.map((priority) => {
-                      return (
-                        <SelectItem value={priority.value} key={priority.value}>
-                          {priority.value}
-                        </SelectItem>
-                      )
-                    })}
-                  </SelectContent>
-                </Select>
-              )}
-            />
-          </div>
-
-          <div className="mb-4 flex flex-col">
-            <p className="text-sm font-semibold text-slate-500">Due Date</p>
-            <DatePicker<z.infer<typeof formSchema>>
-              name="dueDate"
-              control={form.control}
-            />
-          </div>
-
-          <MenuSeparator />
-
-          <div className="flex flex-col">
-            <p className="text-sm font-semibold text-slate-500">Tags</p>
-            <Controller
-              control={form.control}
-              name="tags"
-              render={() => (
-                <MultiSelect
-                  placeholder="Add Tags"
-                  defaultValue={[]}
-                  options={
-                    tags?.map((tag) => ({
-                      label: tag.tag,
-                      value: tag.id,
-                    })) ?? []
-                  }
-                  onValueChange={(values) => {
-                    const selectedOptions = values.map((value) => ({
-                      id: value,
-                      tag: tags?.find((tag) => tag.id === value)?.tag ?? '',
-                    }))
-                    form.setValue('tags', selectedOptions)
-                  }}
-                />
-              )}
-            />
-          </div>
-        </form>
-      </div>
-
-      <div className="flex justify-between">
-        <Button variant="outline">Delete Task</Button>
-        <Button
-          onClick={() => {
-            mutate(form.getValues())
-          }}
-        >
-          Save Changes
-        </Button>
+        <div className="flex justify-between">
+          <Button variant="outline">Delete Task</Button>
+          <Button
+            onClick={() => {
+              mutate(form.getValues())
+            }}
+          >
+            Save Changes
+          </Button>
+        </div>
       </div>
     </div>
   )
