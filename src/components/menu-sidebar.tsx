0945971376e3d543df4ab0e835863bbdeef13989
Richard Schwarcz <riso.schwarcz@gmail.com>
2024-07-20 12:09:57 +0200

    remove width defining container

--- src/components/menu-sidebar.tsx
+++ src/components/menu-sidebar.tsx
@@ -67,119 +67,113 @@ export default function MenuSidebar({
   }, [])
 
   return (
-    <div className="hidden w-72 flex-none lg:block">
-      <div className="flex min-h-[calc(100vh-2rem)] flex-col justify-between rounded-3xl bg-stone-50 p-4">
-        <div>
-          <div className="mb-4 flex items-center justify-between">
-            <p className="text-xl font-bold">Menu</p>
-            <Button
-              size="icon"
-              variant="ghost"
-              onClick={() => setSidebarVisibility(!isSidebarVisible)}
-            >
-              <Menu />
-            </Button>
-          </div>
-
-          <MenuSeparator />
-
-          <div className="mb-4 flex flex-col gap-2">
-            <p className="text-sm font-semibold text-slate-800">TASKS</p>
-            <div className="flex gap-4">
-              <ChevronsRight />
-              <p>Upcoming</p>
-            </div>
-            <div className="flex gap-4">
-              <ListTodo />
-              <p>Today</p>
-            </div>
-          </div>
-
-          <MenuSeparator />
-
-          <div className="mb-4 flex flex-col gap-2">
-            <p className="text-sm font-semibold text-slate-800">LISTS</p>
-            <div>
-              {lists?.map((list) => {
-                return (
-                  <div key={list.id}>
-                    <ListItem color={list.color}>{list.list}</ListItem>
-                  </div>
-                )
-              })}
-            </div>
-
-            {isNewListInputVisible && (
-              <ReactiveInput
-                onMutate={(value) =>
-                  createList({ list: value, color: faker.color.rgb() })
-                }
-                placeholder=""
-              />
-            )}
-
-            <button
-              className="border-bpy-4 flex w-full gap-4 text-left"
-              onClick={() => setNewListInputVisibility(true)}
-            >
-              <PlusIcon />
-              Add New List
-            </button>
-          </div>
-
-          <MenuSeparator />
-
-          <div className="mb-4 flex flex-col gap-2">
-            <p className="text-sm font-semibold text-slate-800">TAGS</p>
-            <div className="flex flex-wrap gap-2">
-              {tags?.map((tag) => {
-                return (
-                  <div
-                    key={tag.id}
-                    className="flex w-fit items-center gap-1 rounded-lg border px-2 py-1"
-                    style={{ backgroundColor: hexToRgba(tag.color, '0.2') }}
-                  >
-                    <p>{tag.tag}</p>
-                    <X className="h-5 w-5 cursor-pointer rounded-sm p-1 hover:bg-red-400" />
-                  </div>
-                )
-              })}
-            </div>
-
-            {isNewTagInputVisible && (
-              <ReactiveInput
-                onMutate={(value) =>
-                  createTag({ tag: value, color: faker.color.rgb() })
-                }
-                placeholder=""
-              />
-            )}
-
-            <button
-              className="border-bpy-4 flex w-full gap-4 text-left"
-              onClick={() => setNewTagInputVisibility(true)}
-            >
-              <PlusIcon />
-              Add New Tag
-            </button>
-          </div>
-        </div>
-
-        <div className="flex gap-2">
-          <Button variant="ghost" size="icon" onClick={() => setTheme('dark')}>
-            <Moon />
-          </Button>
-          <Button variant="ghost" size="icon" onClick={() => setTheme('light')}>
-            <Sun />
-          </Button>
+    <div className="flex min-h-[calc(100vh-2rem)] flex-col justify-between rounded-3xl bg-stone-50 p-4">
+      <div>
+        <div className="mb-4 flex items-center justify-between">
+          <p className="text-xl font-bold">Menu</p>
           <Button
-            variant="ghost"
             size="icon"
-            onClick={() => setTheme('system')}
+            variant="ghost"
+            onClick={() => setSidebarVisibility(!isSidebarVisible)}
           >
-            <Monitor />
+            <Menu />
           </Button>
         </div>
+
+        <MenuSeparator />
+
+        <div className="mb-4 flex flex-col gap-2">
+          <p className="text-sm font-semibold text-slate-800">TASKS</p>
+          <div className="flex gap-4">
+            <ChevronsRight />
+            <p>Upcoming</p>
+          </div>
+          <div className="flex gap-4">
+            <ListTodo />
+            <p>Today</p>
+          </div>
+        </div>
+
+        <MenuSeparator />
+
+        <div className="mb-4 flex flex-col gap-2">
+          <p className="text-sm font-semibold text-slate-800">LISTS</p>
+          <div>
+            {lists?.map((list) => {
+              return (
+                <div key={list.id}>
+                  <ListItem color={list.color}>{list.list}</ListItem>
+                </div>
+              )
+            })}
+          </div>
+
+          {isNewListInputVisible && (
+            <ReactiveInput
+              onMutate={(value) =>
+                createList({ list: value, color: faker.color.rgb() })
+              }
+              placeholder=""
+            />
+          )}
+
+          <button
+            className="border-bpy-4 flex w-full gap-4 text-left"
+            onClick={() => setNewListInputVisibility(true)}
+          >
+            <PlusIcon />
+            Add New List
+          </button>
+        </div>
+
+        <MenuSeparator />
+
+        <div className="mb-4 flex flex-col gap-2">
+          <p className="text-sm font-semibold text-slate-800">TAGS</p>
+          <div className="flex flex-wrap gap-2">
+            {tags?.map((tag) => {
+              return (
+                <div
+                  key={tag.id}
+                  className="flex w-fit items-center gap-1 rounded-lg border px-2 py-1"
+                  style={{ backgroundColor: hexToRgba(tag.color, '0.2') }}
+                >
+                  <p>{tag.tag}</p>
+                  <X className="h-5 w-5 cursor-pointer rounded-sm p-1 hover:bg-red-400" />
+                </div>
+              )
+            })}
+          </div>
+
+          {isNewTagInputVisible && (
+            <ReactiveInput
+              onMutate={(value) =>
+                createTag({ tag: value, color: faker.color.rgb() })
+              }
+              placeholder=""
+            />
+          )}
+
+          <button
+            className="border-bpy-4 flex w-full gap-4 text-left"
+            onClick={() => setNewTagInputVisibility(true)}
+          >
+            <PlusIcon />
+            Add New Tag
+          </button>
+        </div>
+      </div>
+
+      <div className="flex gap-2">
+        <Button variant="ghost" size="icon" onClick={() => setTheme('dark')}>
+          <Moon />
+        </Button>
+        <Button variant="ghost" size="icon" onClick={() => setTheme('light')}>
+          <Sun />
+        </Button>
+        <Button variant="ghost" size="icon" onClick={() => setTheme('system')}>
+          <Monitor />
+        </Button>
       </div>
     </div>
   )
