698c201c222324d1808065f8298a1b5173973575
Richard Schwarcz <riso.schwarcz@gmail.com>
2024-07-20 16:04:09 +0200

    pass taskAction to taskSidebar

--- src/components/task-drawer.tsx
+++ src/components/task-drawer.tsx
@@ -1,14 +1,16 @@
 import { type Dispatch, type SetStateAction } from 'react'
 
 import { Drawer, DrawerContent } from '~/components/ui/drawer'
-import NewTaskSidebar from './new-task-sidebar'
+import TaskSidebar from './task-sidebar'
 
 export function TaskDrawer({
   isDrawerOpen,
   setIsDrawerOpen,
+  taskAction,
 }: {
   isDrawerOpen: boolean
   setIsDrawerOpen: Dispatch<SetStateAction<boolean>>
+  taskAction: 'editTask' | 'newTask'
 }) {
   return (
     <Drawer
@@ -17,9 +19,10 @@ export function TaskDrawer({
       direction="right"
     >
       <DrawerContent className="fixed bottom-0 right-0 h-full w-[400px] bg-stone-50">
-        <NewTaskSidebar
+        <TaskSidebar
           isTaskbarVisible={isDrawerOpen}
           setTaskbarVisibility={setIsDrawerOpen}
+          taskAction={taskAction}
         />
       </DrawerContent>
     </Drawer>
