7a7c297e25be2f0111434f0a83b3916cab2f6389
Richard Schwarcz <riso.schwarcz@gmail.com>
2024-07-21 09:00:37 +0200

    change bg of drawer content

--- src/components/menu-drawer.tsx
+++ src/components/menu-drawer.tsx
@@ -14,7 +14,7 @@ export function MenuDrawer({
   return (
     <Drawer onOpenChange={setIsDrawerOpen} open={isDrawerOpen} direction="left">
       <DrawerContent
-        className="w-full overflow-auto bg-stone-50 sm:w-[400px]"
+        className="w-full overflow-auto bg-card sm:w-[400px]"
         aria-describedby={undefined}
       >
         <DialogTitle className="hidden">Menu sidebar</DialogTitle>
